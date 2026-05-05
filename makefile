8fe9d38c167b44c7bd098e8f137f011577975d76
AnAverageBox <brayanel@udel.edu>
2026-05-05 13:38:15 -0400

    changed .cu files back to .c and updated makefile to allow it

--- makefile
+++ makefile
@@ -1,12 +1,24 @@
-FLAGS= -DDEBUG
+#FLAGS= -DDEBUG
+#LIBS= -lm
+#ALWAYS_REBUILD=makefile
+
+#nbody: nbody.cu compute.cu
+#	nvcc $(FLAGS) $^ -o $@ $(LIBS)
+
+#clean:
+#	rm -f *.o nbody
+
+
+#updated to allow for the new .cu files
+
+#below is to keep the file extensions as .c
+FLAGS= -DDEBUG -x cu
 LIBS= -lm
 ALWAYS_REBUILD=makefile
 
-nbody: nbody.cu compute.cu
+# Note the change from .cu to .c here
+nbody: nbody.c compute.c
 	nvcc $(FLAGS) $^ -o $@ $(LIBS)
 
 clean:
 	rm -f *.o nbody
-
-
-#updated to allow for the new .cu files
--- nbody.c
+++ nbody.c
@@ -7,109 +7,128 @@
 #include "planets.h"
 #include "compute.h"
 
-// represents the objects in the system.  Global variables
-vector3 *hVel, *d_hVel;
-vector3 *hPos, *d_hPos;
-double *mass;
+// host arrays - the CPU side of things
+vector3 *hVel, *hPos;
+double  *mass;
 
-//initHostMemory: Create storage for numObjects entities in our system
-//Parameters: numObjects: number of objects to allocate
-//Returns: None
-//Side Effects: Allocates memory in the hVel, hPos, and mass global variables
+// device arrays - the GPU side (declared extern in vector.h)
+vector3 *d_hVel, *d_hPos;
+double  *d_mass;
+
+// just malloc the three host arrays
 void initHostMemory(int numObjects)
 {
-	hVel = (vector3 *)malloc(sizeof(vector3) * numObjects);
-	hPos = (vector3 *)malloc(sizeof(vector3) * numObjects);
-	mass = (double *)malloc(sizeof(double) * numObjects);
+    hVel = (vector3 *)malloc(sizeof(vector3) * numObjects);
+    hPos = (vector3 *)malloc(sizeof(vector3) * numObjects);
+    mass = (double  *)malloc(sizeof(double)  * numObjects);
 }
 
-//freeHostMemory: Free storage allocated by a previous call to initHostMemory
-//Parameters: None
-//Returns: None
-//Side Effects: Frees the memory allocated to global variables hVel, hPos, and mass.
 void freeHostMemory()
 {
-	free(hVel);
-	free(hPos);
-	free(mass);
+    free(hVel);
+    free(hPos);
+    free(mass);
 }
 
-//planetFill: Fill the first NUMPLANETS+1 entries of the entity arrays with an estimation
-//				of our solar system (Sun+NUMPLANETS)
-//Parameters: None
-//Returns: None
-//Fills the first 8 entries of our system with an estimation of the sun plus our 8 planets.
-void planetFill(){
-	int i,j;
-	double data[][7]={SUN,MERCURY,VENUS,EARTH,MARS,JUPITER,SATURN,URANUS,NEPTUNE};
-	for (i=0;i<=NUMPLANETS;i++){
-		for (j=0;j<3;j++){
-			hPos[i][j]=data[i][j];
-			hVel[i][j]=data[i][j+3];
-		}
-		mass[i]=data[i][6];
-	}
+// allocate GPU memory and push the initial state up to the device
+void initDeviceMemory(int numObjects)
+{
+    cudaMalloc((void**)&d_hPos, sizeof(vector3) * numObjects);
+    cudaMalloc((void**)&d_hVel, sizeof(vector3) * numObjects);
+    cudaMalloc((void**)&d_mass, sizeof(double)  * numObjects);
+
+    cudaMemcpy(d_hPos, hPos, sizeof(vector3) * numObjects, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_hVel, hVel, sizeof(vector3) * numObjects, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_mass, mass, sizeof(double)  * numObjects, cudaMemcpyHostToDevice);
 }
 
-//randomFill: FIll the rest of the objects in the system randomly starting at some entry in the list
-//Parameters: 	start: The index of the first open entry in our system (after planetFill).
-//				count: The number of random objects to put into our system
-//Returns: None
-//Side Effects: Fills count entries in our system starting at index start (0 based)
+void freeDeviceMemory()
+{
+    cudaFree(d_hPos);
+    cudaFree(d_hVel);
+    cudaFree(d_mass);
+}
+
+// pull final positions and velocities back from the GPU so we can print them
+void syncToHost(int numObjects)
+{
+    cudaMemcpy(hPos, d_hPos, sizeof(vector3) * numObjects, cudaMemcpyDeviceToHost);
+    cudaMemcpy(hVel, d_hVel, sizeof(vector3) * numObjects, cudaMemcpyDeviceToHost);
+}
+
+// fill the first NUMPLANETS+1 slots with solar system data
+void planetFill()
+{
+    int i, j;
+    double data[][7] = {SUN,MERCURY,VENUS,EARTH,MARS,JUPITER,SATURN,URANUS,NEPTUNE};
+    for (i = 0; i <= NUMPLANETS; i++) {
+        for (j = 0; j < 3; j++) {
+            hPos[i][j] = data[i][j];
+            hVel[i][j] = data[i][j+3];
+        }
+        mass[i] = data[i][6];
+    }
+}
+
+// throw some random asteroids into the mix
 void randomFill(int start, int count)
 {
-	int i, j, c = start;
-	for (i = start; i < start + count; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			hVel[i][j] = (double)rand() / RAND_MAX * MAX_DISTANCE * 2 - MAX_DISTANCE;
-			hPos[i][j] = (double)rand() / RAND_MAX * MAX_VELOCITY * 2 - MAX_VELOCITY;
-			mass[i] = (double)rand() / RAND_MAX * MAX_MASS;
-		}
-	}
+    int i, j;
+    for (i = start; i < start + count; i++) {
+        for (j = 0; j < 3; j++) {
+            hVel[i][j] = (double)rand() / RAND_MAX * MAX_DISTANCE * 2 - MAX_DISTANCE;
+            hPos[i][j] = (double)rand() / RAND_MAX * MAX_VELOCITY * 2 - MAX_VELOCITY;
+            mass[i]    = (double)rand() / RAND_MAX * MAX_MASS;
+        }
+    }
 }
 
-//printSystem: Prints out the entire system to the supplied file
-//Parameters: 	handle: A handle to an open file with write access to prnt the data to
-//Returns: 		none
-//Side Effects: Modifies the file handle by writing to it.
-void printSystem(FILE* handle){
-	int i,j;
-	for (i=0;i<NUMENTITIES;i++){
-		fprintf(handle,"pos=(");
-		for (j=0;j<3;j++){
-			fprintf(handle,"%lf,",hPos[i][j]);
-		}
-		printf("),v=(");
-		for (j=0;j<3;j++){
-			fprintf(handle,"%lf,",hVel[i][j]);
-		}
-		fprintf(handle,"),m=%lf\n",mass[i]);
-	}
+void printSystem(FILE *handle)
+{
+    int i, j;
+    for (i = 0; i < NUMENTITIES; i++) {
+        fprintf(handle, "pos=(");
+        for (j = 0; j < 3; j++)
+            fprintf(handle, "%lf,", hPos[i][j]);
+        fprintf(handle, "),v=(");
+        for (j = 0; j < 3; j++)
+            fprintf(handle, "%lf,", hVel[i][j]);
+        fprintf(handle, "),m=%lf\n", mass[i]);
+    }
 }
 
 int main(int argc, char **argv)
 {
-	clock_t t0=clock();
-	int t_now;
-	//srand(time(NULL));
-	srand(1234);
-	initHostMemory(NUMENTITIES);
-	planetFill();
-	randomFill(NUMPLANETS + 1, NUMASTEROIDS);
-	//now we have a system.
-	#ifdef DEBUG
-	printSystem(stdout);
-	#endif
-	for (t_now=0;t_now<DURATION;t_now+=INTERVAL){
-		compute();
-	}
-	clock_t t1=clock()-t0;
-#ifdef DEBUG
-	printSystem(stdout);
-#endif
-	printf("This took a total time of %f seconds\n",(double)t1/CLOCKS_PER_SEC);
+    clock_t t0 = clock();
+    int t_now;
 
-	freeHostMemory();
+    srand(1234); // fixed seed so results are reproducible
+    initHostMemory(NUMENTITIES);
+    planetFill();
+    randomFill(NUMPLANETS + 1, NUMASTEROIDS);
+
+#ifdef DEBUG
+    printSystem(stdout);
+#endif
+
+    // ship the initial state to the GPU - it stays there the whole simulation
+    initDeviceMemory(NUMENTITIES);
+
+    for (t_now = 0; t_now < DURATION; t_now += INTERVAL) {
+        compute();
+    }
+
+    // bring results back so we can print them
+    syncToHost(NUMENTITIES);
+
+    clock_t t1 = clock() - t0;
+
+#ifdef DEBUG
+    printSystem(stdout);
+#endif
+    printf("This took a total time of %f seconds\n", (double)t1 / CLOCKS_PER_SEC);
+
+    freeDeviceMemory();
+    freeHostMemory();
+    return 0;
 }
