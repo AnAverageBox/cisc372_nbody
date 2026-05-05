867d52da738946e414e20d89042dca1add3acefb
AnAverageBox <brayanel@udel.edu>
2026-05-05 14:28:17 -0400

    undid randomFill change

--- nbody.cu
+++ nbody.cu
@@ -76,11 +76,8 @@ void randomFill(int start, int count)
     int i, j;
     for (i = start; i < start + count; i++) {
         for (j = 0; j < 3; j++) {
-            //hVel[i][j] = (double)rand() / RAND_MAX * MAX_DISTANCE * 2 - MAX_DISTANCE;
-            //hPos[i][j] = (double)rand() / RAND_MAX * MAX_VELOCITY * 2 - MAX_VELOCITY;
-            //prevents the number from going crazy high in the output
-            hVel[i][j] = (double)rand() / RAND_MAX * MAX_VELOCITY * 2 - MAX_VELOCITY;
-            hPos[i][j] = (double)rand() / RAND_MAX * MAX_DISTANCE * 2 - MAX_DISTANCE;
+            hVel[i][j] = (double)rand() / RAND_MAX * MAX_DISTANCE * 2 - MAX_DISTANCE;
+            hPos[i][j] = (double)rand() / RAND_MAX * MAX_VELOCITY * 2 - MAX_VELOCITY;
             mass[i]    = (double)rand() / RAND_MAX * MAX_MASS;
         }
     }
